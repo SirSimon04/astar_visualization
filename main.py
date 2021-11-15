e5edde21e501b7d174edb882c2e776d583f44c49
SirSimon04 <simi@engelnetz.de>
2021-11-15 21:12:02 +0100

    refactor, can use different algs on same grid

--- main.py
+++ main.py
@@ -1,92 +1,10 @@
 import pygame
-import math
 from queue import PriorityQueue
+from spot import *
 
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("A* Pathfinding")
-
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 255, 0)
-YELLOW = (255, 255, 0)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-PURPLE = (128, 0, 128)
-ORANGE = (255, 165, 0)
-GREY = (128, 128, 128)
-TURQUOISE = (64, 224, 208)
-
-
-class Spot:
-    def __init__(self, row, col, width, total_rows):
-        self.row = row
-        self.col = col
-        self.x = row * width
-        self.y = col * width
-        self.color = WHITE
-        self.neighbors = []
-        self.width = width
-        self.total_rows = total_rows
-
-    def get_pos(self):
-        return self.row, self.col
-
-    def is_closed(self):
-        return self.color == RED
-
-    def is_open(self):
-        return self.color == GREEN
-
-    def is_barrier(self):
-        return self.color == BLACK
-
-    def is_start(self):
-        return self.color == ORANGE
-
-    def is_end(self):
-        return self.color == TURQUOISE
-
-    def reset(self):
-        self.color = WHITE
-
-    def make_closed(self):
-        self.color = RED
-
-    def make_open(self):
-        self.color = GREEN
-
-    def make_barrier(self):
-        self.color = BLACK
-
-    def make_end(self):
-        self.color = TURQUOISE
-
-    def make_start(self):
-        self.color = ORANGE
-
-    def make_path(self):
-        self.color = PURPLE
-
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
-
-    def update_neighbors(self, grid):
-        self.neighbors = []
-        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier():  # DOWN
-            self.neighbors.append(grid[self.row + 1][self.col])
-
-        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():  # UP
-            self.neighbors.append(grid[self.row - 1][self.col])
-
-        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():  # RIGHT
-            self.neighbors.append(grid[self.row][self.col + 1])
-
-        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():  # LEFT
-            self.neighbors.append(grid[self.row][self.col - 1])
-
-    def __lt__(self, other):
-        return False
 
 
 def h(p1, p2):
@@ -106,11 +24,12 @@ def make_grid(rows, width):
     return grid
 
 
-def reconstruct_path(came_from, current, draw):
+def reconstruct_path(came_from, current, draw, start):
     while current in came_from:
-        current = came_from[current]
-        current.make_path()
-        draw()
+        if current is not start:
+            current = came_from[current]
+            current.make_path()
+            draw()
 
 
 def greedy(draw, grid, start, end):
@@ -132,8 +51,9 @@ def greedy(draw, grid, start, end):
         open_set_hash.remove(current)
 
         if current == end:
-            reconstruct_path(came_from, end, draw)
+            reconstruct_path(came_from, end, draw, start)
             end.make_end()
+            start.make_start()
             draw()
             return True
 
@@ -176,8 +96,9 @@ def dijkstra(draw, grid, start, end):
         open_set_hash.remove(current)
 
         if current == end:
-            reconstruct_path(came_from, end, draw)
+            reconstruct_path(came_from, end, draw, start)
             end.make_end()
+            start.make_start()
             draw()
             return True
 
@@ -222,8 +143,9 @@ def a_star(draw, grid, start, end):
         open_set_hash.remove(current)
 
         if current == end:
-            reconstruct_path(came_from, end, draw)
+            reconstruct_path(came_from, end, draw, start)
             end.make_end()
+            start.make_start()
             draw()
             return True
 
@@ -244,7 +166,7 @@ def a_star(draw, grid, start, end):
 
         if current != start:
             current.make_closed()
-
+    start.make_start()
     return False
 
 
@@ -285,6 +207,8 @@ def main(win, width):
 
     run = True
 
+    algo_running = False
+
     while run:
         draw(win, grid, ROWS, width)
         for event in pygame.event.get():
@@ -321,19 +245,31 @@ def main(win, width):
                     for row in grid:
                         for spot in row:
                             spot.update_neighbors(grid)
+                    algo_running = True
                     a_star(lambda: draw(win, grid, ROWS, width), grid, start, end)
+                    algo_running = False
 
                 if event.key == pygame.K_d and start and end:
                     for row in grid:
                         for spot in row:
                             spot.update_neighbors(grid)
+                    algo_running = True
                     dijkstra(lambda: draw(win, grid, ROWS, width), grid, start, end)
+                    algo_running = False
 
                 if event.key == pygame.K_h and start and end:
                     for row in grid:
                         for spot in row:
                             spot.update_neighbors(grid)
+                    algo_running = True
                     greedy(lambda: draw(win, grid, ROWS, width), grid, start, end)
+                    algo_running = False
+
+                if event.key == pygame.K_r and not algo_running:
+                    for row in grid:
+                        for spot in row:
+                            if not spot.is_end() and not spot.is_start() and not spot.is_barrier():
+                                spot.reset()
 
                 if event.key == pygame.K_c:
                     start = None
